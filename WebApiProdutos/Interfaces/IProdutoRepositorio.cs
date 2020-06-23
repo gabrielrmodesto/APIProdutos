352bbffbf9013cda301f36e8911394485d3a1c63
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-22 23:44:04 -0300

    Product controller, repository, interface and model

---
+++ WebApiProdutos/Interfaces/IProdutoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiProdutos.Models;
+
+namespace WebApiProdutos.Interfaces
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> GetAll();
+        Produto Get(int id);
+        Produto Add(Produto item);
+        void Remove(int id);
+        bool Update(Produto item);
+    }
+}
